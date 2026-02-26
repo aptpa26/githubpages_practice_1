8b3de9c57ebbeb10f755f3bf0eb09b378ece9bee
at <mailperlapp@gmail.com>
2026-02-26 14:57:52 +0900

    new

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); // ページリロードを止める
+
+  const username = document.getElementById("user").value;
+  const password = document.getElementById("pass").value;
+
+  // ここでは仮のログインチェック
+  if (username === "user" && password === "pass") {
+    // 成功 → home.htmlへ移動
+    window.location.href = "home.html";
+  } else {
+    alert("ログイン失敗");
+  }
+});
+
+
 const params = new URLSearchParams(location.search);
 const chapterId = params.get('id');
 
