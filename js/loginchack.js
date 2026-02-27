19012cf5076c895cc25bd73ac56aee61dc68a2e2
at <mailperlapp@gmail.com>
2026-02-27 16:40:50 +0900

    new3

--- js/loginchack.js
+++ js/loginchack.js
@@ -1,13 +1,14 @@
-// logincheck.js
-
 const GAS_URL = "https://script.google.com/macros/s/AKfycbwNrCeV86YSoLym46BCSDBDli3k34TN74--TDrnU-vPXOiG_-tnguE_fR81FVcr7DYTSQ/exec";
+
+// ページ読み込み後すぐ実行
+checkLoginAndRedirect();
 
 async function checkLoginAndRedirect() {
   const token = localStorage.getItem("token");
 
   if (!token) {
     console.log("No token → redirecting to login");
-    window.location.href = "index.html"; // login
+    window.location.href = "index.html";
     return;
   }
 
@@ -16,23 +17,15 @@ async function checkLoginAndRedirect() {
       method: "POST",
       headers: { "Content-Type": "text/plain;charset=utf-8" },
       body: JSON.stringify({
-      path: "checkToken",
-      token: token  
+        path: "checkToken",
+        token: token
       })
     });
-    
-    
-    // fetch(`${GAS_URL}?path=checkToken`, {
-    //   method: "POST",
-    //   headers: { "Content-Type": "text/plain;charset=utf-8" },
-    //   body: JSON.stringify({ token })
-    // });
 
     const result = await res.json();
     console.log("Token check:", result);
 
     if (!result.ok) {
-      console.log("Token invalid → redirect to login");
       localStorage.removeItem("token");
       window.location.href = "index.html";
     }
