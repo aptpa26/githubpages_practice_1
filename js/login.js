dec23fec20c6310402e7782a4fb9e4cd9277c61d
at <mailperlapp@gmail.com>
2026-02-28 16:06:38 +0900

    new

--- js/login.js
+++ js/login.js
@@ -2,15 +2,5 @@ const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault(); // ページリロードを止める
-
-  const username = document.getElementById("user").value;
-  const password = document.getElementById("pass").value;
-
-  // ここでは仮のログインチェック
-  if (username === "user" && password === "pass") {
-    // 成功 → home.htmlへ移動
-    window.location.href = "home.html";
-  } else {
-    alert("ログイン失敗");
-  }
+  window.location.href = "home.html";
 });
